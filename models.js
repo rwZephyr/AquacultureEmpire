f70cc09957a736516c5424b701bf3b0032a14153
rwZephyr <ryan.wogan@gmail.com>
2025-07-26 18:52:55 -0700

    persist fishBuffer across saves

--- models.js
+++ models.js
@@ -104,7 +104,8 @@ export class Vessel {
     Object.defineProperty(this, 'harvestTimeout', { value: null, writable: true, enumerable: false });
     Object.defineProperty(this, 'harvestProgress', { value: 0, writable: true, enumerable: false });
     Object.defineProperty(this, 'harvestFishBuffer', { value: 0, writable: true, enumerable: false });
-    Object.defineProperty(this, 'fishBuffer', { value: [], writable: true, enumerable: false });
+    // fishBuffer needs to persist so make it enumerable
+    this.fishBuffer = [];
     Object.defineProperty(this, 'harvestingPenIndex', { value: null, writable: true, enumerable: false });
     Object.defineProperty(this, 'travelInterval', { value: null, writable: true, enumerable: false });
     Object.defineProperty(this, 'offloadInterval', { value: null, writable: true, enumerable: false });
